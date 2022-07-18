9a658da47f853fb1f98d527cb71c29207d770b99
Anusha <anushavysakh232@gmail.com>
2022-07-18 21:37:46 +0530

    uc1 committed

--- LineComparison.java
+++ LineComparison.java
@@ -1,11 +1,23 @@
 package com.Assignment2.Bridlabz;
 
-public class LineComparison {
+import java.util.Scanner;
+
+public class uc1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Line Computation Program");
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first co-ordinate of x");
+		int x1 = sc.nextInt();
+		System.out.println("Enter the first co-ordinate of y");
+		int y1 = sc.nextInt();
+		System.out.println("Enter the second co-ordinate of x");
+		int x2 = sc.nextInt();
+		System.out.println("Enter the second co-ordinate of y");
+		int y2 = sc.nextInt();
+		double line_length = Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+		System.out.println("The length of line is" + line_length);
 	}
 
 }
