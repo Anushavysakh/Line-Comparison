ff6a62d9e09463f18aed6a533f7bc66d26809998
Anusha <anushavysakh232@gmail.com>
2022-07-18 21:40:37 +0530

    uc3 committed

--- LineComparison.java
+++ LineComparison.java
@@ -2,40 +2,40 @@ package com.Assignment2.Bridlabz;
 
 import java.util.Scanner;
 
-public class uc2 {
-	int x1, x2, y1, y2, a1, a2, b1, b2;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		uc2 firstobject = new uc2();
-		firstobject.equal();
+public class uc3 {
+
+	public static double Line(int x1, int x2, int y1, int y2) {
+		return ((double) Math.round(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) * 100)) / 100;
 	}
 
-	public void equal() {
+	public static void main(String[] args) {
+		int x1, x2, y1, y2;
+		System.out.println("Enter First line points x1,y1 and x2,y2");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the first co-ordinate of x1");
-		int x1 = sc.nextInt();
-		System.out.println("Enter the first co-ordinate of y1");
-		int y1 = sc.nextInt();
-		System.out.println("Enter the second co-ordinate of x2");
-		int x2 = sc.nextInt();
-		System.out.println("Enter the second co-ordinate of y2");
-		int y2 = sc.nextInt();
-		System.out.println("Enter the first co-ordinate of a1");
-		int a1 = sc.nextInt();
-		System.out.println("Enter the first co-ordinate of b1");
-		int b1 = sc.nextInt();
-		System.out.println("Enter the second co-ordinate of a2");
-		int a2 = sc.nextInt();
-		System.out.println("Enter the second co-ordinate of yb2");
-		int b2 = sc.nextInt();
-		double length1 = Math.sqrt(Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2)));
-		double length2 = Math.sqrt(Math.pow((a2 - a1), 2) + (Math.pow((b2 - b1), 2)));
-		if (length1 == length2) {
-			System.out.println("Lines are equal");
-		} else {
-			System.out.println("Lines are not equal");
-
-		}
+		x1 = sc.nextInt();
+		y1 = sc.nextInt();
+		x2 = sc.nextInt();
+		y2 = sc.nextInt();
+		double line1 = Line(x1, x2, y1, y2);
+		int X1, X2, Y1, Y2;
+		System.out.println("Enter Second line points X1,Y1 and X2,Y2");
+		X1 = sc.nextInt();
+		Y1 = sc.nextInt();
+		X2 = sc.nextInt();
+		Y2 = sc.nextInt();
+		double line2 = Line(X1, X2, Y1, Y2);
+		sc.close();
+		System.out.println("distancebetween" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") is " + line1);
+		System.out.println("distancebetween" + "(" + X1 + "," + Y1 + ")," + "(" + X2 + "," + Y2 + ") is " + line2);
+		
+		
+		if (line1 > line2)
+			System.out.println("Line1 is greater than Line2");
+		else if (line1 < line2)
+			System.out.println("Line2 is greater than Line1");
+		else
+			System.out.println("Line1 and line2 are equal");
+			
 	}
 }
