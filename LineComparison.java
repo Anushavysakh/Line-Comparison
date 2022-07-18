d8e9d9504c85abaeacac253cd675415a88ffad45
Anusha <anushavysakh232@gmail.com>
2022-07-18 21:34:25 +0530

    first commit

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package com.Assignment2.Bridlabz;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Computation Program");
+		
+	}
+
+}
